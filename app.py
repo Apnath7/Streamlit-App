201db01ba41374e5c22c59932513d40f7f412dc6
Nathaniel <nathanielsalvoro@gmail.com>
2024-09-01 20:15:57 +0800

    added

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.write("### BMI Calculator ###")
+st.title("Nathaniel Salvoro")
+st.caption("I am Nathaniel Salvoro, and I'm a fourth-year college student studying Bachelor of Science in Information Technology. After I graduate, I want to become a successful Web Developer.")
+st.divider()
+
+st.header("BMI Calculator")
+st.caption("a simple app I made")
+
 
 height = st.slider("Enter your height", 0, 250, 175)
 weight = st.slider("Enter your weight", 0, 500, 70)
@@ -8,7 +14,7 @@ weight = st.slider("Enter your weight", 0, 500, 70)
 bmi = weight / ((height/100) ** 2)
 
 st.write(f"BMI: {bmi:.2f}")
-
+st.divider()
 st.write("### CLASSIFICATION ###")
 st.write("Underweight: less than 18.5")
 st.write("Normal: 18.5 - 24.9")
